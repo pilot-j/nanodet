0211f45b6b829afd3f18acdf48d8ef9129e9beb9
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-07-27 22:30:06 +0530

    Update GsPAN.py

--- nanodet/model/fpn/GsPAN.py
+++ nanodet/model/fpn/GsPAN.py
@@ -1,1 +1,57 @@
+import torch
+import torch.nn as nn
 
+from ..backbone.gsConv import Conv, DWConv, GSConv, GSBottleneck
+from ..module.conv import ConvModule, DepthwiseConvModule
+
+
+class GSBlocks(nn.Module):
+    """Stack of GhostBottleneck used in GhostPAN.
+
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        expand (int): Expand ratio of GhostBottleneck. Default: 1.
+        kernel_size (int): Kernel size of depthwise convolution. Default: 5.
+        num_blocks (int): Number of GhostBottlecneck blocks. Default: 1.
+        use_res (bool): Whether to use residual connection. Default: False.
+        activation (str): Name of activation function. Default: LeakyReLU.
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        expand =1,
+        kernel_size=3,
+        num_blocks=1,
+        use_res=False,
+        activation="LeakyReLU",
+    ):
+        super(GSBlocks, self).__init__()
+        self.use_res = use_res
+        if use_res:
+            self.reduce_conv = ConvModule(
+                in_channels,
+                out_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                activation=activation,
+            )
+        blocks = []
+        for _ in range(num_blocks):
+            blocks.append(
+                GSBottleneck(
+                    in_channels,
+                    int(out_channels * expand),
+                    dw_kernel_size=kernel_size
+                )
+            )
+        self.blocks = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        out = self.blocks(x)
+        if self.use_res:
+            out = out + self.reduce_conv(x)
+        return out
