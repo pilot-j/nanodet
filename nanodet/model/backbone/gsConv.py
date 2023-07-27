c5791fabc09f198799911a6c2e47a33dcba5da03
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-07-27 22:06:03 +0530

    Update gsConv.py

--- nanodet/model/backbone/gsConv.py
+++ nanodet/model/backbone/gsConv.py
@@ -3,7 +3,20 @@ import math
 import torch
 import torch.nn as nn
 
-#from utils.activations import MemoryEfficientMish as Mish
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = Mish() if act else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+
 
 
 def autopad(k, p=None):  # kernel, padding
