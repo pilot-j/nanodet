481b9f286a9a1c9d7ad00d3cc5601ef79bd78c84
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-07-28 19:00:26 +0530

    Update gsConv.py

--- nanodet/model/backbone/gsConv.py
+++ nanodet/model/backbone/gsConv.py
@@ -42,11 +42,6 @@ class GSConv(nn.Module):
     def forward(self, x):
         x1 = self.cv1(x)
         x2 = torch.cat((x1, self.cv2(x1)), 1)
-        # shuffle
-        # y = x2.reshape(x2.shape[0], 2, x2.shape[1] // 2, x2.shape[2], x2.shape[3])
-        # y = y.permute(0, 2, 1, 3, 4)
-        # return y.reshape(y.shape[0], -1, y.shape[3], y.shape[4])
-
         b, n, h, w = x2.data.size()
         b_n = b * n // 2
         y = x2.reshape(b_n, 2, h * w)
@@ -78,7 +73,7 @@ class GSBottleneck(nn.Module):
         # for lighting
         self.conv_lighting = nn.Sequential(
             GSConv(c1, c_, 1, 1),
-            GSConv(c_, c2, 3, 1, act=False))
+            GSConvns(c_, c2, 3, 1, act=False))
         self.shortcut = Conv(c1, c2, 1, 1, act=False)
 
     def forward(self, x):
@@ -95,16 +90,19 @@ class GSBottleneckC(GSBottleneck):
 
 class VoVGSCSP(nn.Module):
     # VoVGSCSP module with GSBottleneck
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+    def __init__(self, c1, c2,shortcut=True, g=1, e=0.5):
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
-        # self.gc1 = GSConv(c_, c_, 1, 1)
-        # self.gc2 = GSConv(c_, c_, 1, 1)
-        # self.gsb = GSBottleneck(c_, c_, 1, 1)
-        self.gsb = nn.Sequential(*(GSBottleneck(c_, c_, e=1.0) for _ in range(n)))
+        self.gc1 = GSConv(c_, c_, 1, 1)
+        self.gc2 = GSConv(c_, c_, 1, 1)
+        self.gsb = GSBottleneck(c_, c_, 1, 1)
+        
         self.res = Conv(c_, c_, 3, 1, act=False)
-        self.cv3 = Conv(2 * c_, c2, 1)  #
-
+        self.cv3 = Conv(2 * c_, c2, 1)
+    def forward(self, x):
+        x1 = self.gsb(self.cv1(x))
+        y = self.cv2(x)
+        return self.cv3(torch.cat((y, x1), dim=1))
 
