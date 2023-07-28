3c305095973aa6c813d2558b142d990403942a18
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-07-28 13:34:10 +0530

    Update efficientnet_lite.py

--- nanodet/model/backbone/efficientnet_lite.py
+++ nanodet/model/backbone/efficientnet_lite.py
@@ -7,6 +7,27 @@ from torch import nn
 
 from ..module.activation import act_layers
 
+#SPPF From yolov5
+class SPPF(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(SPPF, self).__init__()
+
+        c_ = int(in_channels//2)
+        
+        self.c1 = nn.Conv2d(in_channels, c_, 1, 1, 0)
+        self.pool = nn.MaxPool2d(kernel_size=5, stride=1, padding=2)
+        self.c_out = nn.Conv2d(c_ * 4, out_channels, 1, 1, 0)
+        self.batch_norm1 = nn.BatchNorm2d(c_)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.batch_norm1(self.c1(x))
+        pool1 = self.pool(x)
+        pool2 = self.pool(pool1)
+        pool3 = self.pool(pool2)
+
+        return self.batch_norm2(self.c_out(torch.cat([x, pool1, pool2, pool3], dim=1)))
+
 efficientnet_lite_params = {
     # width_coefficient, depth_coefficient, image_size, dropout_rate
     "efficientnet_lite0": [1.0, 1.0, 224, 0.2],
