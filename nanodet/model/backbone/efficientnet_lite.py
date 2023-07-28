83f60f00cf176ea5522466e2e8721d6f48eb7ae1
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-07-29 00:35:56 +0530

    Update efficientnet_lite.py

--- nanodet/model/backbone/efficientnet_lite.py
+++ nanodet/model/backbone/efficientnet_lite.py
@@ -6,31 +6,6 @@ import torch.utils.model_zoo as model_zoo
 from torch import nn
 
 from ..module.activation import act_layers
-class LayerNorm(nn.Module):
-    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
-    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
-    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
-    with shape (batch_size, channels, height, width).
-    """
-    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.eps = eps
-        self.data_format = data_format
-        if self.data_format not in ["channels_last", "channels_first"]:
-            raise NotImplementedError 
-        self.normalized_shape = (normalized_shape, )
-    
-    def forward(self, x):
-        if self.data_format == "channels_last":
-            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-        elif self.data_format == "channels_first":
-            u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)
-            x = (x - u) / torch.sqrt(s + self.eps)
-            x = self.weight[:, None, None] * x + self.bias[:, None, None]
-            return x
 #SPPF From yolov5
 class SPPF(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -230,12 +205,10 @@ class EfficientNetLite(nn.Module):
             [1, 3, 1, 6, 192, 320, 0.25],  # stage6 - 1/32
         ]
 
-        # Stem ---> inspired by ConvNext
-        pad = (207*
         out_channels = 32
         self.stem = nn.Sequential(
-            nn.Conv2d(3, out_channels, kernel_size=4, stride=4, padding=416, bias=False),
-            nn.LayerNorm(out_channels, eps=epsilon, elementwise_affine=False, track_running_stats=False),
+            nn.Conv2d(3, out_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=out_channels, momentum=momentum, eps=epsilon),
             act_layers(activation),
         )
 
