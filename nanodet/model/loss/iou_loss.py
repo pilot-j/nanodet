aa9727353e2dfb77585d5e712c147ef86ee53690
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-07-30 00:33:00 +0530

    implemented eiou loss

--- nanodet/model/loss/iou_loss.py
+++ nanodet/model/loss/iou_loss.py
@@ -347,8 +347,63 @@ def ciou_loss(pred, target, eps=1e-7):
     cious = ious - (rho2 / c2 + v**2 / (1 - ious + v))
     loss = (1 - cious) #focal cious
     return loss
+    
+@weighted_loss
+def eiou_loss(pred, target, eps=1e-7):
+    r"""`Implementation of paper `Focal and Efficient IOU Loss for Accurate Bounding Box Regression
+    https://arxiv.org/pdf/2101.08158.pdf
+    Args:
+        pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
+            shape (n, 4).
+        target (Tensor): Corresponding gt bboxes, shape (n, 4).
+        eps (float): Eps to avoid log(0).
+    Return:
+        Tensor: Loss tensor.
+    """
+    # overlap
+    lt = torch.max(pred[:, :2], target[:, :2])
+    rb = torch.min(pred[:, 2:], target[:, 2:])
+    wh = (rb - lt).clamp(min=0)
+    overlap = wh[:, 0] * wh[:, 1]
 
+    # union
+    ap = (pred[:, 2] - pred[:, 0]) * (pred[:, 3] - pred[:, 1])
+    ag = (target[:, 2] - target[:, 0]) * (target[:, 3] - target[:, 1])
+    union = ap + ag - overlap + eps
 
+    # IoU
+    ious = overlap / union
+
+    # enclose area
+    enclose_x1y1 = torch.min(pred[:, :2], target[:, :2])
+    enclose_x2y2 = torch.max(pred[:, 2:], target[:, 2:])
+    enclose_wh = (enclose_x2y2 - enclose_x1y1).clamp(min=0)
+
+    cw = enclose_wh[:, 0]
+    ch = enclose_wh[:, 1]
+
+    c2 = cw**2 + ch**2 + eps
+
+    b1_x1, b1_y1 = pred[:, 0], pred[:, 1]
+    b1_x2, b1_y2 = pred[:, 2], pred[:, 3]
+    b2_x1, b2_y1 = target[:, 0], target[:, 1]
+    b2_x2, b2_y2 = target[:, 2], target[:, 3]
+
+    w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+    w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+    del_w = ((w1-w2)/(cw+eps))**2
+    del_h = ((h1-h2)/(cw+eps))**2
+    left = ((b2_x1 + b2_x2) - (b1_x1 + b1_x2)) ** 2 / 4
+    right = ((b2_y1 + b2_y2) - (b1_y1 + b1_y2)) ** 2 / 4
+    rho2 = left + right
+
+    
+    # EIoU
+    eious = ious - (rho2 / c2 + del_w +del_h)
+    loss = (1 - eious) 
+    return loss
+class EIOU(nn.Module):
+    pass
 class IoULoss(nn.Module):
     """IoULoss.
 
@@ -537,6 +592,38 @@ class CIoULoss(nn.Module):
         assert reduction_override in (None, "none", "mean", "sum")
         reduction = reduction_override if reduction_override else self.reduction
         loss = self.loss_weight * ciou_loss(
+            pred,
+            target,
+            weight,
+            eps=self.eps,
+            reduction=reduction,
+            avg_factor=avg_factor,
+            **kwargs,
+        )
+        return loss
+class EIoULoss(nn.Module):
+    def __init__(self, eps=1e-6, reduction="mean", loss_weight=1.0):
+        super(EIoULoss, self).__init__()
+        self.eps = eps
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def forward(
+        self,
+        pred,
+        target,
+        weight=None,
+        avg_factor=None,
+        reduction_override=None,
+        **kwargs,
+    ):
+        if weight is not None and not torch.any(weight > 0):
+            if pred.dim() == weight.dim() + 1:
+                weight = weight.unsqueeze(1)
+            return (pred * weight).sum()  # 0
+        assert reduction_override in (None, "none", "mean", "sum")
+        reduction = reduction_override if reduction_override else self.reduction
+        loss = self.loss_weight * eiou_loss(
             pred,
             target,
             weight,
