6294e7e7a6ad63a2d2f87e26b87a3cf18cf5900c
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-08-05 21:25:03 +0530

    Update custom_csp.py

--- nanodet/model/backbone/custom_csp.py
+++ nanodet/model/backbone/custom_csp.py
@@ -60,44 +60,11 @@ class TinyResBlock(nn.Module):
             return self.out_conv(x + x1)
         else:
             return torch.cat((x1, x), dim=1)
-class GhostModuleV2(nn.Module):
-    def __init__(self, inp, oup, kernel_size=1, ratio=2, dw_size=3, stride=1, relu=True,args=None):
-        super(GhostModuleV2, self).__init__()
-        self.mode=mode
-        self.gate_fn=nn.Sigmoid() 
-        self.oup = oup
-        init_channels = math.ceil(oup / ratio) 
-        new_channels = init_channels*(ratio-1)
-        self.primary_conv = nn.Sequential(  
-                nn.Conv2d(inp, init_channels, kernel_size, stride, kernel_size//2, bias=False),
-                nn.BatchNorm2d(init_channels),
-                nn.ReLU(inplace=True) if relu else nn.Sequential(),
-        )
-        self.cheap_operation = nn.Sequential(
-                nn.Conv2d(init_channels, new_channels, dw_size, 1, dw_size//2, groups=init_channels, bias=False),
-                nn.BatchNorm2d(new_channels),
-                nn.ReLU(inplace=True) if relu else nn.Sequential(),
-        ) 
-        self.short_conv = nn.Sequential( 
-                nn.Conv2d(inp, oup, kernel_size, stride, kernel_size//2, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.Conv2d(oup, oup, kernel_size=(1,5), stride=1, padding=(0,2), groups=oup,bias=False),
-                nn.BatchNorm2d(oup),
-                nn.Conv2d(oup, oup, kernel_size=(5,1), stride=1, padding=(2,0), groups=oup,bias=False),
-                nn.BatchNorm2d(oup),
-        )
-      
-    def forward(self, x):
-         res=self.short_conv(F.avg_pool2d(x,kernel_size=2,stride=2))  
-         x1 = self.primary_conv(x)
-         x2 = self.cheap_operation(x1)
-         out = torch.cat([x1,x2], dim=1)
-         return out[:,:self.oup,:,:]*F.interpolate(self.gate_fn(res),size=(out.shape[-2],out.shape[-1]),mode='nearest')
 class TinyResBlock_attn(nn.Module):
     def __init__(
         self, in_channels, kernel_size, norm_cfg, activation, res_type="concat"
     ):
-        super(TinyResBlock, self).__init__()
+        super(TinyResBlock_attn, self).__init__()
         assert in_channels % 2 == 0
         assert res_type in ["concat", "add"]
         self.res_type = res_type
@@ -144,7 +111,7 @@ class CspBlock1(nn.Module):
         norm_cfg=dict(type="BN", requires_grad=True),
         activation="LeakyReLU",
     ):
-        super(CspBlock, self).__init__()
+        super(CspBlock1, self).__init__()
         assert in_channels % 2 == 0
         self.in_conv = ConvModule(
             in_channels,
@@ -185,7 +152,7 @@ class CspBlock2(nn.Module):
         norm_cfg=dict(type="BN", requires_grad=True),
         activation="LeakyReLU",
     ):
-        super(CspBlock, self).__init__()
+        super(CspBlock2, self).__init__()
         assert in_channels % 2 == 0
         self.in_conv = ConvModule(
             in_channels,
