6eb0294818a6f82fee8d834b3b8a92c0df306ee9
pilot-j <120032562+pilot-j@users.noreply.github.com>
2023-08-06 22:30:40 +0530

    Update enhanced_shufflenet.py

--- nanodet/model/backbone/enhanced_shufflenet.py
+++ nanodet/model/backbone/enhanced_shufflenet.py
@@ -16,8 +16,95 @@ import torch
 import torch.nn as nn
 
 from ..module.conv import ConvModule
+def _make_divisible(v, divisor, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
 
 
+def hard_sigmoid(x, inplace: bool = False):
+    if inplace:
+        return x.add_(3.0).clamp_(0.0, 6.0).div_(6.0)
+    else:
+        return F.relu6(x + 3.0) / 6.0
+def channel_shuffle(x, groups):
+    # type: (torch.Tensor, int) -> torch.Tensor
+    batchsize, num_channels, height, width = x.data.size()
+    channels_per_group = num_channels // groups
+
+    # reshape
+    x = x.view(batchsize, groups, channels_per_group, height, width)
+
+    x = torch.transpose(x, 1, 2).contiguous()
+
+    # flatten
+    x = x.view(batchsize, -1, height, width)
+
+    return x
+class SqueezeExcite(nn.Module):
+    def __init__(
+        self,
+        in_chs,
+        se_ratio=0.25,
+        reduced_base_chs=None,
+        activation="ReLU",
+        gate_fn=hard_sigmoid,
+        divisor=4,
+        **_
+    ):
+        super(SqueezeExcite, self).__init__()
+        self.gate_fn = gate_fn
+        reduced_chs = _make_divisible((reduced_base_chs or in_chs) * se_ratio, divisor)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv_reduce = nn.Conv2d(in_chs, reduced_chs, 1, bias=True)
+        self.act1 = act_layers(activation)
+        self.conv_expand = nn.Conv2d(reduced_chs, in_chs, 1, bias=True)
+
+    def forward(self, x):
+        x_se = self.avg_pool(x)
+        x_se = self.conv_reduce(x_se)
+        x_se = self.act1(x_se)
+        x_se = self.conv_expand(x_se)
+        x = x * self.gate_fn(x_se)
+        return x
+class InvertedRes(nn.Module):
+    def __init__(self, in_ch, mid_ch, out_ch, stride):
+        super().__init__()
+        self.act = nn.ReLU()
+        self.conv_pw = nn.Sequential(
+            nn.Conv2d(in_ch//2, mid_ch//2,kernel_size =1, stride =1, padding =0, groups =1),
+            BatchNorm2d(mid_ch//2)
+        )
+        self.conv_dw = nn.Sequential(
+            nn.Conv2d(mid_ch//2, mid_ch//2,kernel_size =3, stride =stride, padding =1, groups =mid_ch//2),
+            BatchNorm2d(mid_ch//2)
+        )
+        self.SE = SqueezeExcite(mid_ch)
+        self.conv_linear = nn.Sequential(
+            nn.Conv2d(mid_ch, out_ch//2, kernel_size = 1, stride =1, padding =0, groups =1)
+            nn.BatchNorm(out_ch//2)
+        )
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        x2 = self.act(self.conv_pw(x2))
+        x3 = self.act(self.conv_dw(x2))
+        x3 = torch.concat((x2,x3), dim=1)
+        x3 = self.act(self.conv_linear(self.SE(x3)))
+        out = torch.cat((x1,x3), dim=1)
+        out = channel_shuffle(out,2)
+        return out
+        
+        
 class TinyResBlock(nn.Module):
     def __init__(
         self, in_channels, kernel_size, norm_cfg, activation, res_type="concat"
